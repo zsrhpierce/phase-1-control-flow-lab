b944be8a59915b7d4e563ca66a75ad7e44231467
Cathy Pierce <zsrhpierce@gmail.com>
2023-08-01 10:02:52 -0600

    control

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <=400) {
+  return 'This one is on me!';
+  } else if (ride >400 && ride < 2000) {
+    return 'That will be twenty bucks.';
+  } else if (ride >= 2000 && ride < 2500){
+    return'I will gladly take your thirty bucks.';
+  } else if (ride > 2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let cityCheck = cityName === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityCheck;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipPaid){   
+  let tipMessage;
+
+  switch (tipPaid){
+    case 'generous':
+      tipMessage = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipMessage = "Thank you.";
+      break;
+    default:
+      tipMessage = "Bye.";
+      break;
+  }
+  return tipMessage;
 }
